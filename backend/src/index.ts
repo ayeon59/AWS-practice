d68387303bc470667cd86baa4831ca0ccf38d1e5
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2026-01-21 18:37:15 +0900

    set mysql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+
+dotenv.config();
 
 const app = express();
 const PORT = 80;
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME!,
+  process.env.DATABASE_USERNAME!,
+  process.env.DATABASE_PASSWORD!,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+  }
+);
 
 app.get("/", (_req, res) => {
   res.send("Hello! 백엔드 서버가 정상 작동 중입니다 🚀");
@@ -11,6 +25,12 @@ app.get("/health", (req, res) => {
   res.status(200).send("Success Heatlth Check");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("DB 연결 성공!");
+  } catch (err) {
+    console.log("DB 연결 X", err);
+  }
   console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
 });
